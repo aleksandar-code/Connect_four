45caee646b97fc145b2f4ba4c2562337523ea659
aleksandar-code <aca.jevdjevic@gmail.com>
2022-10-27 14:05:06 +0200

    Update spacing

--- spec/players_spec.rb
+++ spec/players_spec.rb
@@ -5,39 +5,40 @@ require_relative './players'
 require_relative './game'
 
 Rspec.describe Players do
-    describe '#play' do
-      context 'when' do
-        it 'returns' do
-        end
-      end
-    end
-    
-    describe '#play' do
-      context 'when' do
-        it 'returns' do
-        end
-      end
-    end
 
-    describe '#play' do
-      context 'when' do
-        it 'returns' do
-        end
+  describe '#play' do
+    context 'when' do
+      it 'returns' do
       end
     end
+  end
+  
+  describe '#play' do
+    context 'when' do
+      it 'returns' do
+      end
+    end
+  end
+  
+  describe '#play' do
+    context 'when' do
+      it 'returns' do
+      end
+    end
+  end
 
-    describe '#play' do
-      context 'when' do
-        it 'returns' do
-        end
+  describe '#play' do
+    context 'when' do
+      it 'returns' do
       end
     end
+  end
 
-    describe '#play' do
-      context 'when' do
-        it 'returns' do
-        end
+  describe '#play' do
+    context 'when' do
+      it 'returns' do
       end
     end
+  end
 
 end
