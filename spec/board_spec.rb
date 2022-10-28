c9f583c671fa15e2cf654ff1d12719c48ddf50fb
aleksandar-code <aca.jevdjevic@gmail.com>
2022-10-28 10:47:12 +0200

    Setup test for place_at

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,12 +4,12 @@ require_relative '../lib/board'
 require_relative '../lib/players'
 
 RSpec.describe Board do
-  
+  subject(:board_test) { described_class.new }
   describe '#place_at' do
     context 'when a location and a symbol are given' do
   
       it 'change the board observable state' do
-        
+        expect { board_test.place_at(1, "✦") }.to change { board_test.board.last.first }.to("✦")
       end
   
     end
